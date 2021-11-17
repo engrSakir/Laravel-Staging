caf2d7829d6dbf321d0c13f9cdabf72716cc792b
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-11-17 16:24:48 +0600

    Import and export
    composer require maatwebsite/excel

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Yoeunes\Toastr\ToastrServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
         Arcanedev\LogViewer\LogViewerServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -232,6 +233,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
