b779274e10768c984c52c7baa04768c59f9a4546
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-11-17 17:24:23 +0600

    product-import-export

--- routes/backend.php
+++ routes/backend.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,3 +11,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('product', [ProductController::class, 'index'])->middleware(['auth'])->name('product.index');
+Route::get('product-export', [ProductController::class, 'export'])->middleware(['auth'])->name('product.export');
+Route::post('product-import', [ProductController::class, 'import'])->middleware(['auth'])->name('product.import');
